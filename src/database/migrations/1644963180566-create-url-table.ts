2d725ff1e18fbc21543e4496e4daff457894843c
Daniel Adegoke <danyadegoke@gmail.com>
2022-02-15 23:53:14 +0100

    first: pus

--- src/database/migrations/1644963180566-create-url-table.ts
+++ src/database/migrations/1644963180566-create-url-table.ts
@@ -1,4 +1,4 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class createUrlTable1644963180566 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
